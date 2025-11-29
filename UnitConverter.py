f0c2967752231d14060762400a0ad0efc9c64929
sachin47043 <sachin829235@gmail.com>
2025-11-29 18:06:03 +0530

    Add files via upload
    This project contains a beginner-friendly Python Unit Converter that performs multiple real-life conversions based on user input. The script handles distance, currency, measurement, and time conversions using simple arithmetic formulas.

---
+++ UnitConverter.py
@@ -0,0 +1,18 @@
+k=float(input("ENTER THE DISTANCE IN KILOMETRES : "))
+m = (k * 1000)
+print("Your distance in meter: ", m, "m")
+
+#DOLLAR TO INR 
+d = float(input("ENTER THE AMOUNT IN DOLLARS : "))
+INR = (88.5*d)
+print("Your AMOUNT in INR : Rs", INR)
+
+#inches to km
+inches = int(input("ENTER THE inches  : "))
+kilometres = (inches*0.0000254)
+print("Your inches in kilometres : ", kilometres, "km")
+
+#Hour To Seconds 
+hours = int(input("ENTER THE TIME IN HOURS : " ))
+seconds = (hours*3600)
+print("Your enterd Time  in Seconds : ",seconds,"s")
