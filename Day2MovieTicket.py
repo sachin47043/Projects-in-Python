b0f5caa009c42345ab1843ba817976b36dd18a02
sachin47043 <sachin829235@gmail.com>
2025-11-29 16:52:21 +0530

    Add files via upload
    This project contains a simple Python program that checks whether a user is allowed to watch a movie based on their age. The script uses multiple conditional statements to handle different scenarios, such as adult entry, entry with a parent, and ticket verification.

---
+++ Day2MovieTicket.py
@@ -0,0 +1,42 @@
+age = int(input("Enter your age"))
+if age > 18 :
+    print("You can watch the movie")
+else :
+    print("Not allowed")
+    
+    
+    
+    age = int(input("Enter your age"))
+ticket = True
+if age > 18 :
+    print("You can watch the movie")
+elif age < 15 :
+    print("You can watch the movie with parent")
+else :
+    print("Not allowed")
+    
+    
+    
+    age = int(input("Enter your age"))
+ticket = True
+if age > 18 :
+    print("You can watch the movie")
+    if age > 18 :
+        print("check identity")
+elif age < 15 :
+    print("You can watch the movie with parent")
+else :
+    print("Not allowed")
+    
+    
+    
+    age = int(input("Enter your age"))
+ticket = True
+if age > 18 :
+    print("You can watch the movie")
+    if ticket == True :
+        print("check ticket and allow him to enter")
+elif age < 15 :
+    print("You can watch the movie with parent")
+else :
+    print("Not allowed")
